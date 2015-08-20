13d989176327e13ee6f1ceaea22e4faf02055e87
Tyler Lemburg <trlemburg@gmail.com>
2015-08-20 12:15:00 -0500

    Update ruby v

--- unicorn.rb
+++ unicorn.rb
@@ -1,6 +1,3 @@
 worker_processes 1
 working_directory '/var/www/html/innovationstudio-manager.unl.edu'
 pid '/var/run/innovationstudio-manager-unicorn.pid'
-
-stderr_path '/var/log/innovationstudio-manager_err.log'
-stdout_path '/var/log/innovationstudio-manager_out.log'
