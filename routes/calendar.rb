b1fc7e3d7ee0f1160706c7f9de96b451572d02c1
Tyler Lemburg <trlemburg@gmail.com>
2015-08-23 12:48:08 -0500

    Better calendar styling

--- routes/calendar.rb
+++ routes/calendar.rb
@@ -4,10 +4,12 @@ get '/calendar/' do
 	@breadcrumbs << {:text => 'Calendar'}
 
 	# get all events for this week
-	events = Event.includes(:event_type).where(:service_space_id => SS_ID).in_week(Time.now).all
-	sunday = Time.now.in_time_zone.sunday
+	date = params[:date].nil? ? Time.now : Time.parse(params[:date])
+	events = Event.includes(:event_type).where(:service_space_id => SS_ID).in_week(date).all
+	sunday = date.in_time_zone.sunday
 
 	erb :calendar, :layout => :fixed, :locals => {
+		:date => date,
 		:sunday => sunday,
 		:events => events
 	}
