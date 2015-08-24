c5ff97db2c9864e38dfe26272895998d1d6f33d0
Tyler Lemburg <trlemburg@gmail.com>
2015-08-24 08:45:37 -0500

    Bugfix on calendar

--- routes/calendar.rb
+++ routes/calendar.rb
@@ -6,7 +6,7 @@ get '/calendar/' do
 	# get all events for this week
 	date = params[:date].nil? ? Time.now : Time.parse(params[:date])
 	events = Event.includes(:event_type).where(:service_space_id => SS_ID).in_week(date).all
-	sunday = date.in_time_zone.sunday
+	sunday = date.in_time_zone.week_start
 
 	erb :calendar, :layout => :fixed, :locals => {
 		:date => date,
