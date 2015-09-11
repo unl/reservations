5b967c13085585d078e51808e70a7f09e9a54765
Tyler Lemburg <trlemburg@gmail.com>
2015-09-11 13:42:57 -0500

    Tool reservations

--- models/reservation.rb
+++ models/reservation.rb
@@ -8,4 +8,12 @@ class Reservation < ActiveRecord::Base
 		tomorrow = (time + 1.day).midnight
 		where('(start_time >= ? AND start_time < ?) OR (end_time >= ? AND end_time < ?)', today, tomorrow, today, tomorrow)
 	}
+
+	# returns length in minutes. If start or end is nil, returns 0
+	def length
+		if end_time.nil? || start_time.nil?
+			return 0
+		end
+		((end_time - start_time) / 60).to_i
+	end
 end
--- routes/tools.rb
+++ routes/tools.rb
@@ -3,6 +3,7 @@ require 'models/reservation'
 require 'models/event'
 require 'models/event_type'
 require 'models/event_signup'
+require 'models/space_hour'
 
 get '/tools/?' do
 	@breadcrumbs << {:text => 'Tools'}
@@ -70,9 +71,17 @@ get '/tools/:resource_id/reserve/?' do
 		redirect '/tools/'
 	end
 
+	date = Time.now.midnight.in_time_zone
+	# get the studio's hours for this day
+	space_hour = SpaceHour.where(:service_space_id => SS_ID)
+		.where('effective_date <= ?', date.utc.strftime('%Y-%m-%d %H:%M:%S')).where(:day_of_week => date.wday)
+		.order(:effective_date => :desc, :id => :desc).first
+
 	erb :reserve, :layout => :fixed, :locals => {
 		:tool => tool,
-		:reservations => Reservation.where(:resource_id => tool.id).in_day(Time.now).all
+		:reservations => Reservation.where(:resource_id => tool.id).in_day(date).all,
+		:space_hour => space_hour,
+		:day => date
 	}
 end
 
@@ -104,18 +113,64 @@ post '/tools/:resource_id/reserve/?' do
 		redirect '/tools/'
 	end
 
-	start = calculate_time(params[:date], params[:start_time_hour], params[:start_time_minute], params[:start_time_am_pm])
+	start_time = calculate_time(params[:date], params[:start_time_hour], params[:start_time_minute], params[:start_time_am_pm])
+	end_time = start_time + params[:length].to_i.minutes
+
+	date = start_time.midnight
+	# validate that the requested time slot falls within the open hours of the day
+	# get the studio's hours for this day
+	space_hour = SpaceHour.where(:service_space_id => SS_ID)
+		.where('effective_date <= ?', date.utc.strftime('%Y-%m-%d %H:%M:%S')).where(:day_of_week => date.wday)
+		.order(:effective_date => :desc, :id => :desc).first
+
+	unless space_hour.nil?
+		# figure out where the closed sections need to be
+        # we can assume that all records in this space_hour are non-intertwined
+        closed_start = 0
+        closed_end = 0
+        starts = space_hour.hours.map{|record| record[:start]}
+        ends = space_hour.hours.map{|record| record[:end]}
+        closeds = []
+        (0..1439).each do |j|
+            if starts.include?(j)
+                closed_end = j
+                closeds << {:status => 'closed', :start => closed_start, :end => closed_end}
+                closed_start = 0
+                closed_end = 0
+            end
+            if ends.include?(j)
+                closed_start = j
+            end
+        end 
+        closed_end = 1440
+        closeds << {:status => 'closed', :start => closed_start, :end => closed_end}
+
+		# for each record, ensure that the time does not overlap if the record is not "open"
+		(space_hour.hours + closeds).each do |record|
+			if record[:status] != 'open'
+				start_time_minutes = 60 * start_time.hour + start_time.min
+				end_time_minutes = 60 * end_time.hour + end_time.min
+				if (record[:start]+1..record[:end]-1).include?(start_time_minutes) || (record[:start]+1..record[:end]-1).include?(end_time_minutes) ||
+						(start_time_minutes < record[:start] && end_time_minutes > record[:end])
+					# there is an overlap, this time is invalid
+					flash :alert, 'Invalid Time Slot', 'Sorry, that time slot is invalid for reservations.'
+					redirect back
+				end
+			end
+		end
+	end
+	# if no record studio is open
 
 	Reservation.create(
 		:resource_id => tool.id,
 		:event_id => nil,
-		:start_time => start,
-		:end_time => start + params[:length].to_i.minutes,
+		:start_time => start_time,
+		:end_time => end_time,
 		:is_training => false,
 		:user_id => @user.id
 	)
 
-	flash(:success, 'Reservation Created', "You have successfully reserved #{tool.name} for #{params[:length]} minutes at #{start.in_time_zone.strftime('%A, %B %d at %l:%M %P')}")
+	flash(:success, 'Reservation Created', "You have successfully reserved #{tool.name} for #{params[:length]} minutes at #{start_time.in_time_zone.strftime('%A, %B %d at %l:%M %P')}")
 	redirect '/tools/'
 end
 
