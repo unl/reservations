397f244ca36e333152c09f5087768c7cd3dccc03
Tyler Lemburg <trlemburg@gmail.com>
2015-11-05 10:21:48 -0600

    Add UTC check in in_day, in_week queries

--- models/event.rb
+++ models/event.rb
@@ -9,15 +9,15 @@ class Event < ActiveRecord::Base
 	alias_method :signups, :event_signups
 
 	scope :in_day, ->(time) {
-		today = time.midnight
-		tomorrow = (time + 1.day).midnight
-		where('(start_time >= ? AND start_time < ?) OR (end_time >= ? AND end_time < ?)', today, tomorrow, today, tomorrow)
+		today = time.in_time_zone.midnight
+		tomorrow = (time.in_time_zone.midnight + 1.day + 1.hour).in_time_zone.midnight
+		where('(start_time >= ? AND start_time < ?) OR (end_time >= ? AND end_time < ?)', today.getutc, tomorrow.getutc, today.getutc, tomorrow.getutc)
 	}
 
 	scope :in_week, ->(time) {
-		last_sunday = time.week_start
-		next_sunday = (time + 1.week).week_start
-		where('(start_time >= ? AND start_time < ?) OR (end_time >= ? AND end_time < ?)', last_sunday, next_sunday, last_sunday, next_sunday)
+		last_sunday = time.in_time_zone.week_start
+		next_sunday = (time.in_time_zone.week_start + 1.week + 1.hour).in_time_zoneweek_start
+		where('(start_time >= ? AND start_time < ?) OR (end_time >= ? AND end_time < ?)', last_sunday.getutc, next_sunday.getutc, last_sunday.getutc, next_sunday.getutc)
 	}
 
 	# returns length in minutes. If start or end is nil, returns 0
--- models/reservation.rb
+++ models/reservation.rb
@@ -6,9 +6,9 @@ class Reservation < ActiveRecord::Base
 	belongs_to :user
 
 	scope :in_day, ->(time) {
-		today = time.midnight
-		tomorrow = (time + 1.day).midnight
-		where('(start_time >= ? AND start_time < ?) OR (end_time >= ? AND end_time < ?)', today, tomorrow, today, tomorrow)
+		today = time.in_time_zone.midnight
+		tomorrow = (time.in_time_zone.midnight + 1.day + 1.hour).in_time_zone.midnight
+		where('(start_time >= ? AND start_time < ?) OR (end_time >= ? AND end_time < ?)', today.getutc, tomorrow.getutc, today.getutc, tomorrow.getutc)
 	}
 
 	# returns length in minutes. If start or end is nil, returns 0
