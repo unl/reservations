33336b0412ca4d71faf32925ea0aef025638f231
Tyler Lemburg <trlemburg@gmail.com>
2016-01-08 15:01:10 -0600

    Add an admin home page

--- routes/admin.rb
+++ routes/admin.rb
@@ -1,3 +1,9 @@
+require 'models/user'
+require 'models/event'
+require 'models/resource'
+require 'models/space_hour'
+require 'models/permission'
+
 def calculate_time(date_string, hour, minute, am_pm)
 	hour ||= 0
 	minute ||= 0
@@ -14,6 +20,36 @@ end
 
 before '/admin*' do
 	raise Sinatra::NotFound unless !@user.nil? && @user.is_admin?
+end
+
+get '/admin/?' do
+	@breadcrumbs << {:text => 'Admin Home'}
+	user_count = User.where(:service_space_id => SS_ID).count
+	upcoming_event_count = Event.where(:service_space_id => SS_ID).where('start_time >= ?', Time.now).count
+	tool_count = Resource.where(:service_space_id => SS_ID).count
+
+	date = Time.now.midnight
+	# get the hours for this day to show
+	hours = SpaceHour.where(:service_space_id => SS_ID)
+		.where('effective_date <= ?', date.utc.strftime('%Y-%m-%d %H:%M:%S'))
+		.order(:effective_date => :desc, :id => :desc).all.to_a
+
+	correct_hour = nil
+
+	hours.each do |space_hour|
+		if date.wday == space_hour.day_of_week && (space_hour.effective_date.in_time_zone.midnight == date.in_time_zone.midnight || (!space_hour.one_off && space_hour.effective_date.in_time_zone.midnight <= date.in_time_zone.midnight))
+			correct_hour = space_hour
+			break
+		end
+	end
+
+	erb :'admin/home', :layout => :fixed, :locals => {
+		:user_count => user_count,
+		:upcoming_event_count => upcoming_event_count,
+		:tool_count => tool_count,
+		:space_hour => correct_hour,
+		:date => date
+	}
 end
 
 Dir.glob("#{ROOT}/routes/admin/*.rb") { |file| require file }
