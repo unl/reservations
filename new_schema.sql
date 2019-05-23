7bf982ae2922613e754bc8315b4ec8236106858c
Jeff Sturek <jsturek@gmail.com>
2019-05-23 09:31:00 -0500

    Request a maker: Preview Commit  (#14)
    Start of maker request implementation

---
+++ new_schema.sql
@@ -0,0 +1,17 @@
+
+
+CREATE TABLE IF NOT EXISTS `maker_requests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `status_id` int(11) NOT NULL,
+  `requestor_name` varchar(255) NOT NULL,
+  `requestor_email` varchar(255) NOT NULL,
+  `requestor_phone` varchar(50) DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`uuid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
---
+++ routes/maker_request.rb
@@ -0,0 +1,226 @@
+require 'models/maker_request'
+
+now_in_lincoln = Time.use_zone("America/Chicago") do
+  Time.current.strftime('%Y-%m-%d %H:%M:%S')
+end
+
+get '/maker_request/?' do
+    @breadcrumbs << {:text => 'Create Maker Request'}
+
+    erb :maker_request, :layout => :fixed, :locals => {
+        maker_request: Maker_Request.new(),
+        extras: {confirm_email: ''}
+    }
+end
+
+get '/maker_request/:maker_request_id/view/?' do
+    @breadcrumbs << {:text => 'View Maker Request'}
+
+    maker_request = Maker_Request.find(params[:maker_request_id])
+    # can only view open requests
+    not_found if maker_request.nil? || maker_request.status_id != Maker_Request::STATUS_OPEN
+
+    erb :maker_request_view, :layout => :fixed, :locals => {
+        maker_request: maker_request
+    }
+end
+
+get '/maker_request/:maker_request_uuid/edit/?' do
+    @breadcrumbs << {:text => 'Edit Maker Request'}
+
+    maker_request = Maker_Request.find_by(uuid: params[:maker_request_uuid])
+    not_found if maker_request.nil?
+
+    erb :maker_request, :layout => :fixed, :locals => {
+        maker_request: maker_request,
+        extras: {
+            confirm_email: maker_request.requestor_email,
+            confirm_read_disclaimer: 1
+        }
+    }
+end
+
+get '/maker_request/:maker_request_uuid/manage/?' do
+    @breadcrumbs << {:text => 'Manage Maker Request'}
+
+    maker_request = Maker_Request.find_by(uuid: params[:maker_request_uuid])
+    not_found if maker_request.nil?
+
+    erb :maker_request_manage, :layout => :fixed, :locals => {
+        maker_request: maker_request
+    }
+end
+
+get '/maker_request/:maker_request_uuid/open/?' do
+    maker_request = Maker_Request.find_by(uuid: params[:maker_request_uuid])
+    not_found if maker_request.nil?
+
+    unless maker_request.status_id == Maker_Request::STATUS_OPEN
+        maker_request.status_id = Maker_Request::STATUS_OPEN
+        maker_request.updated = now_in_lincoln
+        maker_request.save
+    end
+
+    flash :success, 'Maker Request Opened', 'Your maker request has been opened.'
+    redirect "/maker_request/#{maker_request.uuid}/manage/"
+end
+
+get '/maker_request/:maker_request_uuid/close/?' do
+    maker_request = Maker_Request.find_by(uuid: params[:maker_request_uuid])
+    not_found if maker_request.nil?
+
+    unless maker_request.status_id == Maker_Request::STATUS_CLOSED
+        maker_request.status_id = Maker_Request::STATUS_CLOSED
+        maker_request.updated = now_in_lincoln
+        maker_request.save
+    end
+
+    flash :success, 'Maker Request Closed', 'Your maker request has been closed.'
+    redirect "/maker_request/#{maker_request.uuid}/manage/"
+end
+
+get '/maker_request/:maker_request_uuid/delete/?' do
+
+    maker_request = Maker_Request.find_by(uuid: params[:maker_request_uuid])
+    not_found if maker_request.nil?
+
+    maker_request.destroy
+
+    flash :success, 'Maker Request Deleted', 'Your maker request has been deleted.'
+    if @user.nil?
+        redirect '/maker_request/'
+    else
+        redirect '/maker_request/list/'
+    end
+end
+
+post '/maker_request/?' do
+
+    extras = {
+        confirm_email: params[:confirm_email],
+        confirm_read_disclaimer: params[:confirm_read_disclaimer]
+    }
+    params.delete('confirm_email')
+    params.delete('confirm_read_disclaimer')
+
+    maker_request = Maker_Request.new(params)
+
+    errors = maker_request.form_validate(extras)
+
+    if errors.empty?
+        maker_request.uuid = sprintf("%20.10f", Time.now.to_f).delete('.').to_i.to_s(36)
+        maker_request.status_id = Maker_Request::STATUS_OPEN
+        maker_request.created = now_in_lincoln
+        maker_request.updated = now_in_lincoln
+        maker_request.save
+
+        # send email to requestor
+        email_subject = 'Innovation Studio Manager Maker Request'
+        email_body = "#{maker_request.requestor_name},<br><br>Your Innovation Studio Manager Maker Request has been posted.  "\
+            "Please keep this email for you records. You may manage your request here: http://#{request.host}/maker_request/#{maker_request.uuid}/manage"
+        Emailer.mail(maker_request.requestor_email, email_subject, email_body)
+
+        flash :success, 'Maker Request Created', "Your maker request has been created. You should receive a confirmation email at #{params[:requestor_email]}."
+        redirect "/maker_request/#{maker_request.uuid}/manage/"
+    end
+
+    # display errors
+    errors.each { |error| flash :alert, error[:heading], error[:message] }
+
+    erb :maker_request, :layout => :fixed, :locals => {
+        maker_request: maker_request,
+        extras: extras
+    }
+end
+
+post '/maker_request/:maker_request_uuid/edit/?' do
+    maker_request = Maker_Request.find_by(uuid: params[:maker_request_uuid])
+    not_found if maker_request.nil?
+
+    # set updated attributes
+    maker_request.requestor_name =  params[:requestor_name]
+    maker_request.requestor_email =  params[:requestor_email]
+    maker_request.requestor_phone =  params[:requestor_phone]
+    maker_request.category_id =  params[:category_id]
+    maker_request.description =  params[:description]
+    maker_request.updated = now_in_lincoln
+
+    extras = {
+        confirm_email: params[:confirm_email],
+        confirm_read_disclaimer: params[:confirm_read_disclaimer]
+    }
+
+    errors = maker_request.form_validate(extras)
+
+    if errors.empty?
+        maker_request.save
+
+        flash :success, 'Maker Request Saved', 'Your maker request has been saved.'
+        redirect "/maker_request/#{maker_request.uuid}/manage/"
+    end
+
+    # display errors
+    errors.each { |error| flash :alert, error[:heading], error[:message] }
+
+    erb :maker_request, :layout => :fixed, :locals => {
+        maker_request: maker_request,
+        extras: extras
+    }
+end
+
+
+get '/maker_request/list/?' do
+    @breadcrumbs << {:text => 'Maker Request List'}
+    require_login
+
+    maker_requests = Maker_Request.where(status_id: Maker_Request::STATUS_OPEN).all
+    erb :maker_request_list, :layout => :fixed, :locals => {
+        maker_requests: maker_requests
+    }
+end
+
+get '/maker_request/lookup/?' do
+    @breadcrumbs << {:text => 'Maker Request Lookup'}
+
+    lookup_email = ''
+    lookup_email = @user.email unless @user.nil?
+
+    maker_requests = []
+    unless lookup_email.empty?
+        maker_requests = Maker_Request.where(requestor_email: lookup_email).all
+    end
+
+    erb :maker_request_lookup, :layout => :fixed, :locals => {
+        lookup_email: lookup_email,
+        maker_requests: maker_requests
+    }
+end
+
+post '/maker_request/lookup/?' do
+    maker_requests = []
+    if params[:lookup_email].nil? || params[:lookup_email].strip.empty?
+        flash :alert, 'Lookup Email', 'Please provide an email to lookup'
+    elsif !params[:lookup_email].strip.match(Maker_Request::VALID_EMAIL_REGEX)
+        flash :alert, 'Lookup Email', 'Please provide a valid email to lookup'
+    else
+        maker_requests = Maker_Request.where(requestor_email: params[:lookup_email].strip).all
+        unless maker_requests.empty?
+            # send email to requestor
+            email_subject = 'Innovation Studio Manager Maker Request Lookup'
+            email_body = 'The following maker requests are associated to your email:<ul>'
+            maker_requests.each do |maker_request|
+                email_body << "<li>#{maker_request.title} - http://#{request.host}/maker_request/#{maker_request.uuid}/manage</li>"
+                email_body << "<br>"
+            end
+            email_body << "</ul>"
+            Emailer.mail(params[:lookup_email].strip, email_subject, email_body)
+        end
+        # Send success whether email had requests or not so email cannot be validated via form
+        flash :success, 'Lookup Email Sent', "An email with all of your maker requests has been sent to #{params[:lookup_email]} if you had any."
+    end
+
+    redirect back
+end
+
+
+
