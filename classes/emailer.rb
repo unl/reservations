791df6675ec6e57068ccabd8bc88460cf96385c2
Jeff Sturek <jsturek@gmail.com>
2021-02-25 14:30:31 -0600

    Chunk bulk email bcc list to < 2kB limit

--- classes/emailer.rb
+++ classes/emailer.rb
@@ -1,17 +1,52 @@
 require 'pony'
 
 class Emailer
+  RECIPIENT_BYTE_LIMIT = 2048
+
   def self.mail(to, subject, body, bcc = "", attachments = nil)
-    Pony.mail({
-      :to => to,
-      :bcc => bcc,
-      :subject => subject,
-      :html_body => body,
-      :from => 'innovationstudio@unl.edu',
-      :via => self.method,
-      :via_options => self.method_options,
-      :attachments => attachments
-    })
+    if (bcc.bytesize > RECIPIENT_BYTE_LIMIT)
+      bcc_groups = self.breakup_recipient(bcc)
+      bcc_groups.each do |bcc_group|
+        Pony.mail({
+          :to => to,
+          :bcc => bcc_group,
+          :subject => subject,
+          :html_body => body,
+          :from => 'innovationstudio@unl.edu',
+          :via => self.method,
+          :via_options => self.method_options,
+          :attachments => attachments
+        })
+      end
+    else
+      Pony.mail({
+        :to => to,
+        :bcc => bcc,
+        :subject => subject,
+        :html_body => body,
+        :from => 'innovationstudio@unl.edu',
+        :via => self.method,
+        :via_options => self.method_options,
+        :attachments => attachments
+      })
+    end
+  end
+
+  def self.breakup_recipient(recipient)
+    recipients = recipient.split(',').uniq
+    recipient_groups = Array.new
+    list = test_list = ''
+    recipients.each do |r|
+      test_list << "#{r.strip},"
+      if test_list.bytesize > RECIPIENT_BYTE_LIMIT
+        recipient_groups.push(list.chomp(','))
+        list = test_list = ''
+      else
+        list = test_list
+      end
+    end
+    recipient_groups.push(list.chomp(',')) unless list.empty?
+    recipient_groups
   end
 
   private 
