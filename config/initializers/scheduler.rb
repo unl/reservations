ad18021b31ff7247e48d790829638e8236280e64
Josh Jacobs <54561791+jjacobs10@users.noreply.github.com>
2022-11-03 15:06:25 -0500

    Dev membership expiration reminder (#45)
    * Merge stable into expiration reminder (#43)
    
    * Improved member search capabilities (#26)
    
    * Improved search capabilties on manage users page. (#22)
    Added searching by first name, last name, expiration date, and email. Fixed searching by tool authorization.
    
    * Added membership heading feature
    
    * Replaced if statement with ternary operator, removed magic number and uneccesary spaces.
    
    * Added username column to the membership table (#29)
    
    * Added username column to the member table.
    
    * Updated member relationship dropdown, connected new_member_signup fields to the database.
    
    * Updated while loop to be more readable.
    
    * fixed the username parameters to start the numbers at 2
    
    * add service space id for user, default space status: expired
    
    * Updated membership heading with small edits based on feedback. (#34)
    
    * Added pagination to the search member table. (#33)
    
    * Removed commented out code
    
    * Revert "Added pagination to the search member table. (#33)" (#36)
    
    This reverts commit 187fb83be600fb6ec6d15d53c6cbec05697cc842.
    
    * Revert "Merge branch 'dev-membership-expiration-reminder' into stable"
    
    This reverts commit 050c9012833eebc8d02ebfecd94b9af97db3f95c, reversing
    changes made to 8d42830e66bc43ed530711739fbf40736a64b079.
    
    * Dev active membership heading (#38)
    
    * Updated membership heading with small edits based on feedback.
    
    * Updated color on membership heading for close to expiration date.
    
    * Added a null check to the updated membership heading.
    
    * Removed config.json and added to.gitignore
    
    Co-authored-by: Austin Dobrusky <37594225+adobrusky@users.noreply.github.com>
    Co-authored-by: Hadley Susie <hadleysusie123@gmail.com>
    Co-authored-by: Hadley Susie <hsusie2@huskers.unl.edu>
    Co-authored-by: Hadley Susie <69990293+hadleysusie@users.noreply.github.com>
    Co-authored-by: Evan Mielak <evanmielak@gmail.com>
    Co-authored-by: Evan Mielak <45115074+evanmielak@users.noreply.github.com>
    Co-authored-by: adobrusky <austindobrusky@gmail.com>
    
    * Finalized changes. Fixed some bugs
    
    * Made Service Space ID parameter dynamic
    
    Co-authored-by: Austin Dobrusky <37594225+adobrusky@users.noreply.github.com>
    Co-authored-by: Hadley Susie <hadleysusie123@gmail.com>
    Co-authored-by: Hadley Susie <hsusie2@huskers.unl.edu>
    Co-authored-by: Hadley Susie <69990293+hadleysusie@users.noreply.github.com>
    Co-authored-by: Evan Mielak <evanmielak@gmail.com>
    Co-authored-by: Evan Mielak <45115074+evanmielak@users.noreply.github.com>
    Co-authored-by: adobrusky <austindobrusky@gmail.com>

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,9 +3,10 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler::singleton
 
-scheduler.cron '0 12 * * *' do    # Every day at 12 pm
+scheduler.cron '0 12 * * * America/Chicago' do    # Every day at 12 pm Chicago time
     system("ruby ././scripts/email_expiring_users.rb")
 end
 
+# prevents scheduler from exiting
 while 1 do
 end
--- scripts/email_expiring_users.rb
+++ scripts/email_expiring_users.rb
@@ -8,14 +8,17 @@ require 'utils/database'
 
 # script-necessary things
 require 'models/user'
+require 'models/service_space'
+
+SS_ID = ServiceSpace.where(:name => 'Innovation Studio').first.id
 
 # specify how many days before expiration
 # a user should receive a reminder.
 days_before_sending_first_reminder = 3
 days_before_sending_second_reminder = 0
 
-users_expiring_first_reminder = User.where(:service_space_id => 1).where('expiration_date >= ? AND expiration_date < ?', (Time.now + days_before_sending_first_reminder.days) - 1.days, Time.now + days_before_sending_first_reminder.days).all
-users_expiring_second_reminder = User.where(:service_space_id => 1).where('expiration_date >= ? AND expiration_date < ?', (Time.now + days_before_sending_second_reminder.days) - 1.days, Time.now + days_before_sending_second_reminder.days).all
+users_expiring_first_reminder = User.where(:service_space_id => SS_ID).where('expiration_date >= ? AND expiration_date < ?', (Time.now + days_before_sending_first_reminder.days) - 1.days, Time.now + days_before_sending_first_reminder.days).all
+users_expiring_second_reminder = User.where(:service_space_id => SS_ID).where('expiration_date >= ? AND expiration_date < ?', (Time.now + days_before_sending_second_reminder.days) - 1.days, Time.now + days_before_sending_second_reminder.days).all
 
 users_expiring_first_reminder.each do |user|
 	user.send_membership_expiring_email
