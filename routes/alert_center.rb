316c803034e103768c886572239974e7c48d147d
taylor-runge <taylor.dl.runge@gmail.com>
2023-02-07 13:35:02 -0600

    added UI to alert_center to bring it more inline with the mockups.

--- routes/alert_center.rb
+++ routes/alert_center.rb
@@ -1,9 +1,27 @@
 get '/alert_center/?' do
 
-    totals = ["hello", "world"]
+    testAlerts = ["Alert One", "Alert Two", "Alert Three"]
 
     erb :alert_center, :layout => :fixed, :locals => {
-        :totals => totals
+        :generalAlerts => testAlerts,
+        :woodShopAlerts => testAlerts,
+        :metalShopAlerts => testAlerts,
+        :digitalFabricationAlerts => testAlerts,
+        :artAlerts => testAlerts
+    }
+
+end
+
+post '/alert_center/?' do
+
+    testAlerts = ["Alert One", "Alert Two", "Alert Three"]
+
+    erb :alert_center, :layout => :fixed, :locals => {
+        :generalAlerts => testAlerts,
+        :woodShopAlerts => testAlerts,
+        :metalShopAlerts => testAlerts,
+        :digitalFabricationAlerts => testAlerts,
+        :artAlerts => testAlerts
     }
 
 end
