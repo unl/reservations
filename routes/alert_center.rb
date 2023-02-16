84a11a0bee9a83fb9f75b3d029e1aeb15d82a441
taylor-runge <taylor.dl.runge@gmail.com>
2023-02-16 16:10:43 -0600

    Made it so that alert center is auto populated by the created alerts. Also added some scafolding code for the storage of user alerts.

--- routes/alert_center.rb
+++ routes/alert_center.rb
@@ -1,27 +1,43 @@
+require 'models/alert'
+require 'models/alert_signup'
+
+
 get '/alert_center/?' do
 
-    testAlerts = ["Alert One", "Alert Two", "Alert Three"]
+    # if AlertSignup.all.where(user: ) is nil
+    #     userAlerts = AlertSignup.create(user: , alert: nil)
+
+    generalAlerts = Alert.all.where(category_id: 1).to_a
+	generalAlerts.sort_by! {|generalAlerts| generalAlerts.name.downcase + generalAlerts.description.downcase}
+
+    woodShopAlerts = Alert.all.where(category_id: 2).to_a
+	woodShopAlerts.sort_by! {|woodShopAlerts| woodShopAlerts.name.downcase + woodShopAlerts.description.downcase}
+
+    metalShopAlerts = Alert.all.where(category_id: 3).to_a
+	metalShopAlerts.sort_by! {|metalShopAlerts| metalShopAlerts.name.downcase + metalShopAlerts.description.downcase}
+
+    digitalFabricationAlerts = Alert.all.where(category_id: 4).to_a
+	generalAlerts.sort_by! {|digitalFabricationAlerts| digitalFabricationAlerts.name.downcase + digitalFabricationAlerts.description.downcase}
+
+    artAlerts = Alert.all.where(category_id: 5).to_a
+	artAlerts.sort_by! {|artAlerts| artAlerts.name.downcase + artAlerts.description.downcase}
 
     erb :alert_center, :layout => :fixed, :locals => {
-        :generalAlerts => testAlerts,
-        :woodShopAlerts => testAlerts,
-        :metalShopAlerts => testAlerts,
-        :digitalFabricationAlerts => testAlerts,
-        :artAlerts => testAlerts
+        :generalAlerts => generalAlerts,
+        :woodShopAlerts => woodShopAlerts,
+        :metalShopAlerts => metalShopAlerts,
+        :digitalFabricationAlerts => digitalFabricationAlerts,
+        :artAlerts => artAlerts
     }
 
 end
 
 post '/alert_center/?' do
 
-    testAlerts = ["Alert One", "Alert Two", "Alert Three"]
+    # userAlerts = AlertSignup.find_by(user:)
+    # userAlerts.alert = params.value
+    # userAlerts.save
 
-    erb :alert_center, :layout => :fixed, :locals => {
-        :generalAlerts => testAlerts,
-        :woodShopAlerts => testAlerts,
-        :metalShopAlerts => testAlerts,
-        :digitalFabricationAlerts => testAlerts,
-        :artAlerts => testAlerts
-    }
+    redirect '/home/'
 
 end
