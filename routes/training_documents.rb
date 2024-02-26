f964f90ce0a17b3bf20b5968e7f11381ad2b8d93
Thomas Neumann <thomasneumann21800@gmail.com>
2024-02-26 16:10:24 -0600

    Updated drupal links

--- routes/training_documents.rb
+++ routes/training_documents.rb
@@ -10,24 +10,6 @@ get '/training-documents/sop/?' do
 	@breadcrumbs << {:text => 'SOP'}
 
 	erb :sop_training_document, :layout => :fixed, :locals => {
-		:content => Net::HTTP.get_response(URI.parse("https://innovationstudio.unl.edu/node/#{drupal_link_lookup(:sop_training_doc)}?format=partial")).body
-	}
-end
-
-get '/training-documents/workshops/?' do
-	@breadcrumbs << {:text => 'Training Documents', :href => '/training-documents'}
-	@breadcrumbs << {:text => 'Workshops'}
-
-	erb :workshops_training_document, :layout => :fixed, :locals => {
-		:content => Net::HTTP.get_response(URI.parse("https://innovationstudio.unl.edu/node/#{drupal_link_lookup(:workshops_training_doc)}?format=partial")).body
-	}
-end
-
-get '/training-documents/tips-and-tricks/?' do
-	@breadcrumbs << {:text => 'Training Documents', :href => '/training-documents'}
-	@breadcrumbs << {:text => 'Tips and Tricks'}
-
-	erb :tips_and_tricks_training_document, :layout => :fixed, :locals => {
-		:content => Net::HTTP.get_response(URI.parse("https://innovationstudio.unl.edu/node/#{drupal_link_lookup(:tips_training_doc)}?format=partial")).body
+		:content => Net::HTTP.get_response(URI.parse("https://innovationstudio.unl.edu#{drupal_link_lookup(:sop_training_doc)}?format=partial")).body
 	}
 end
