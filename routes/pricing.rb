4cad361da189191ab4ae732dd3c0df40449fab02
Thomas Neumann <thomasneumann21800@gmail.com>
2024-02-26 15:16:28 -0600

    Added pricing from db to page

--- routes/pricing.rb
+++ routes/pricing.rb
@@ -1,10 +1,10 @@
-require 'net/http'
+require 'models/material_price'
 
 get '/pricing/?' do
 	@breadcrumbs << {:text => 'Material Pricing'}
+	material_prices = Material_Price.where(:service_space_id => SS_ID).order(category: :asc, subcategory: :asc, name: :asc).all.to_a
 
 	erb :pricing, :layout => :fixed, :locals => {
-		:content => Net::HTTP.get_response(URI.parse("https://innovationstudio.unl.edu/node/#{drupal_node_lookup(:material_pricing)}?format=partial")).body
+		:material_prices => material_prices
 	}
-
 end
