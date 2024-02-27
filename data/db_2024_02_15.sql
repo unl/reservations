b1ff41864c46cdd3d6a0aac4f68d9122448e1efb
Thomas Neumann <thomasneumann21800@gmail.com>
2024-02-27 11:09:03 -0600

    Moved sql file after rebase

--- data/db_2024_02_15.sql
+++ data/db_2024_02_15.sql
@@ -318,6 +318,27 @@ CREATE TABLE `maker_requests` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `maker_requests`
+--
+
+CREATE TABLE `material_prices` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `category` varchar(255) NOT NULL,
+    `subcategory` varchar(255) DEFAULT NULL,
+    `name` varchar(255) NOT NULL,
+    `price_cents` int(11) NOT NULL,
+    `price_per_unit` varchar(255) DEFAULT NULL,
+    `service_space_id` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `maker_requests`
+--
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `permissions`
 --
 
