d498f980c5a8fa1ee10199d4c38a2e3ce65f1ae8
Thomas Neumann <thomasneumann21800@gmail.com>
2024-10-01 11:53:28 -0500

    Update get response to follow redirects and catch errors when they happen

--- routes/training_documents.rb
+++ routes/training_documents.rb
@@ -7,13 +7,51 @@ get '/training-documents/?' do
 	erb :training_documents, :layout => :fixed
 end
 
+def fetch_final_content(uri)
+	url = URI.parse(uri)
+	
+	begin
+		response = Timeout::timeout(10) { Net::HTTP.get_response(url) } # Timeout after 10 seconds
+	
+		# Follow redirects (if any) until we reach the final destination
+		while response.is_a?(Net::HTTPRedirection)
+			url = URI.parse(response['location'])
+			response = Net::HTTP.get_response(url)
+		end
+
+		# Handle 404 or other HTTP error responses
+		case response
+		when Net::HTTPSuccess
+			response.body
+		when Net::HTTPNotFound
+			raise "Page not found (404)"
+		else
+			raise "HTTP error: #{response.code} #{response.message}"
+		end
+	rescue Timeout::Error
+		raise "Request timed out"
+	rescue => e
+		raise "Failed to fetch content: #{e.message}"
+	end
+end
+
+
 get '/training-documents/sop/?' do
 	not_found if SS_ID != 1
 
-	@breadcrumbs << {:text => 'Training Documents', :href => '/training-documents'}
-	@breadcrumbs << {:text => 'SOP'}
+	@breadcrumbs << { :text => 'Training Documents', :href => '/training-documents' }
+	@breadcrumbs << { :text => 'SOP' }
+
+	begin
+		content = fetch_final_content("https://innovationstudio.unl.edu#{drupal_link_lookup(:sop_training_doc)}?format=partial")
+	rescue => e
+		# Handle CMS failure gracefully (e.g., log error, show custom error message)
+		logger.error "Error fetching content: #{e.message}" # Logging the error
+
+		content = 'Error getting the SOP traning documents'
+	end
 
 	erb :sop_training_document, :layout => :fixed, :locals => {
-		:content => Net::HTTP.get_response(URI.parse("https://innovationstudio.unl.edu#{drupal_link_lookup(:sop_training_doc)}?format=partial")).body
+		:content => content
 	}
 end
