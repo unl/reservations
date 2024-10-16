11873f1154050ed59c47ebcca059dfaa94b6c49a
erohrs2 <erohrs2@huskers.unl.edu>
2024-10-15 21:26:40 -0500

    Create user generates nuid; Checks for Non-NU

--- routes/new_members.rb
+++ routes/new_members.rb
@@ -185,49 +185,51 @@ post '/new_members/sign_up/:event_id/?' do
 
 			# Set the username
 			user.username = username
+			
+			if params[:university_status] != 'Non-NU Student (All Other Institutions)'
+				content = ''
 
-			content = ''
+				# Try to get user uid from directory api
+				begin
+					content = fetch_final_content("https://directory.unl.edu/people/#{username}?format=json")
+				rescue => e
+					# Handle directory API failure gracefully (e.g., log error, show custom error message)
+					logger.error "Could not get user NUID: #{username}" # Logging the error
+					flash(:danger, "Error getting your NUID", "We could not parse your NUID based on your user. If the issue persists, then please contact an administrator.")
+					session[:form_data] = params
+					redirect back
+				end
 
-			# Try to get user uid from directory api
-			begin
-				content = fetch_final_content("https://directory.unl.edu/people/#{username}?format=json")
-			rescue => e
-				# Handle directory API failure gracefully (e.g., log error, show custom error message)
-				logger.error "Could not get user NUID: #{username}" # Logging the error
-				flash(:danger, "Error getting your NUID", "We could not parse your NUID based on your user. If the issue persists, then please contact an administrator.")
-				session[:form_data] = params
-				redirect back
-			end
+				# Check to make sure it is valid json
+				if valid_json?(content) === false
+					logger.error "Could not get user NUID: #{username}" # Logging the error
+					flash(:danger, "Error getting your NUID", "We could not parse your NUID based on your user. If the issue persists, then please contact an administrator.")
+					session[:form_data] = params
+					redirect back
+				end
 
-			# Check to make sure it is valid json
-			if valid_json?(content) === false
-				logger.error "Could not get user NUID: #{username}" # Logging the error
-				flash(:danger, "Error getting your NUID", "We could not parse your NUID based on your user. If the issue persists, then please contact an administrator.")
-				session[:form_data] = params
-				redirect back
-			end
+				# Parse it
+				json_parse_content = JSON.parse(content)
 
-			# Parse it
-			json_parse_content = JSON.parse(content)
+				# Check to make sure we have data and it is formatted right		
+				if json_parse_content.key?('unluncwid') === false || json_parse_content['unluncwid'].empty?
+					logger.error "Could not get user NUID: #{username}" # Logging the error
+					flash(:danger, "Error getting your NUID", "We could not parse your NUID based on your user. If the issue persists, then please contact an administrator.")
+					session[:form_data] = params
+					redirect back
+				end
 
-			# Check to make sure we have data and it is formatted right		
-			if json_parse_content.key?('unluncwid') === false || json_parse_content['unluncwid'].empty?
-				logger.error "Could not get user NUID: #{username}" # Logging the error
-				flash(:danger, "Error getting your NUID", "We could not parse your NUID based on your user. If the issue persists, then please contact an administrator.")
-				session[:form_data] = params
-				redirect back
-			end
+				# Get the nuid and double check we don't have duplicates
+				user_nuid = json_parse_content['unluncwid']
+				unless User.find_by(:user_nuid => user_nuid, :service_space_id => SS_ID).nil?
+					flash(:danger, "Error retrieving your NUID", "A user with that NUID already exists. If you believe this to be an error, please contact an administrator.")
+					session[:form_data] = params
+					redirect back
+				end
 
-			# Get the username and double check we don't have duplicates
-			user_nuid = json_parse_content['unluncwid']
-			unless User.find_by(:user_nuid => user_nuid, :service_space_id => SS_ID).nil?
-				flash(:danger, "Error retrieving your NUID", "A user with that NUID already exists. If you believe this to be an error, please contact an administrator.")
-				session[:form_data] = params
-				redirect back
-			end
-
-			# Set the username
-			user.user_nuid = user_nuid
+				# Set the user nuid
+				user.user_nuid = user_nuid
+		end
 
 		else
 			# Username parameters:
