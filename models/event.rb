906bdb559d1d2822587f8f2aa96c08263c61d51a
erohrs2 <erohrs2@huskers.unl.edu>
2024-10-17 22:17:08 -0500

    Fixed conditional bug with timeless checbox

--- models/event.rb
+++ models/event.rb
@@ -106,7 +106,7 @@ class Event < ActiveRecord::Base
 		self.title = params[:title]
 		self.description = params[:description]
 		self.admin_notes = params[:admin_notes]
-		if params[:timeless_event_checkbox] == "off"
+		if params[:timeless_event_checkbox] != "on"
 			self.start_time = calculate_time(params[:start_date], params[:start_time_hour], params[:start_time_minute], params[:start_time_am_pm])
 			self.end_time = calculate_time(params[:end_date], params[:end_time_hour], params[:end_time_minute], params[:end_time_am_pm])
 		end
--- routes/admin/events.rb
+++ routes/admin/events.rb
@@ -288,7 +288,7 @@ post '/admin/events/create/?' do
 		redirect back
 	end 
 
-	if params[:timeless_event_checkbox] == "0"
+	if params[:timeless_event_checkbox] != "on"
 		if  params[:start_date].blank?
 			flash :error, 'Error', 'Please enter event start date'
 			redirect back
@@ -313,13 +313,13 @@ post '/admin/events/create/?' do
 	event.set_image_data(params)
 	event.set_data(params)
 
-	if event.end_time.in_time_zone < event.start_time.in_time_zone && params[:timeless_event_checkbox] == "off"
+	if event.end_time.in_time_zone < event.start_time.in_time_zone && params[:timeless_event_checkbox] != "on"
 		event.delete
 		flash :alert, "Start and end times create a negative duration.", "Sorry, the selected times cannot be used to create an event. Please try different day or time. Please double check your event information."
 		redirect back
 	end 
 
-	if params.has_key?('reserve_tool') && params['reserve_tool'] == 'on' && !params[:tools].nil? && params[:timeless_event_checkbox] == "off"
+	if params.has_key?('reserve_tool') && params['reserve_tool'] == 'on' && !params[:tools].nil? && params[:timeless_event_checkbox] != "on"
         # check for possible other reservations during this time period
         date = event.start_time.midnight.in_time_zone
         params[:tools].each do |tool_id|
