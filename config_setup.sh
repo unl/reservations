9ea653d36049dbfc3eb619e3756059aaccf2c90c
Ethan <ethaneckhardt58@gmail.com>
2024-10-30 18:55:21 -0500

    revert config setup

--- config_setup.sh
+++ config_setup.sh
@@ -7,4 +7,4 @@ do
         s) secret=${OPTARG};;
     esac
 done
-echo "{\"database\":{\"adapter\":\"mysql2\",\"host\":\"localhost\",\"username\":\"$username\",\"password\":\"$password\",\"database\":\"reservations\"},\"reCaptcha\":{\"site_key\":\"$site\",\"secret_key\":\"$secret\"},\"app\":{\"service_space_id\":\"8\",\"URL\":\"localhost:9393/\",\"cookie_domain\":\"\",\"email_from\":\"innovationstudio@unl.edu\",\"cb_version\":\"2024022702\"}}" > config/server.json
+echo "{\"database\":{\"adapter\":\"mysql2\",\"host\":\"localhost\",\"username\":\"$username\",\"password\":\"$password\",\"database\":\"reservations\"},\"reCaptcha\":{\"site_key\":\"$site\",\"secret_key\":\"$secret\"}}" > config/server.json
