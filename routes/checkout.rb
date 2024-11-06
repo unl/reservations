5bb363e8fa1548083125f9c7b158c07478246b14
Ethan <ethaneckhardt58@gmail.com>
2024-11-06 17:52:20 -0600

    reformatted checkout.erb page added modals to the checkin/checkout tables

--- routes/checkout.rb
+++ routes/checkout.rb
@@ -1,15 +1,15 @@
-require 'models/resource'
-require 'models/reservation'
-require 'models/event'
-require 'models/event_type'
-require 'models/event_signup'
-require 'models/space_hour'
-require 'date'
-require 'erb'
+require "models/resource"
+require "models/reservation"
+require "models/event"
+require "models/event_type"
+require "models/event_signup"
+require "models/space_hour"
+require "date"
+require "erb"
 
-get '/checkout/?' do
-	@breadcrumbs << {:text => 'Checkout'}
-	require_login
+get "/checkout/?" do
+  @breadcrumbs << { :text => "Checkout" }
+  require_login
 
   nuid = params[:nuid]
 
@@ -19,51 +19,48 @@ get '/checkout/?' do
   # user_projects = []
   # user_checked_out = []
   user_projects = [
-    { id: 1, location: 'Garage A', name: 'Project Alpha', last_accessed: DateTime.now - 1 },
-    { id: 2, location: 'Garage B', name: 'Project Beta', last_accessed: DateTime.now - 2 },
-    { id: 3, location: 'Garage C', name: 'Project Gamma', last_accessed: DateTime.now - 3 },
-    { id: 4, location: 'Garage D', name: 'Project Delta', last_accessed: DateTime.now - 4 },
-    { id: 5, location: 'Garage E', name: 'Project Epsilon', last_accessed: DateTime.now - 5 }
+    { id: 1, location: "Garage A", name: "Project Alpha", last_accessed: DateTime.now - 1, description: "Building a robot" },
+    { id: 2, location: "Garage B", name: "Project Beta", last_accessed: DateTime.now - 2, description: "Creating a drone" },
+    { id: 3, location: "Garage C", name: "Project Gamma", last_accessed: DateTime.now - 3, description: "Developing a new app" },
+    { id: 4, location: "Garage D", name: "Project Delta", last_accessed: DateTime.now - 4, description: "Designing a new car model" },
+    { id: 5, location: "Garage E", name: "Project Epsilon", last_accessed: DateTime.now - 5, description: "Constructing a bridge" },
   ]
   user_checked_out = [
-    { id: 1, name: 'Hammer', checked_out_date: DateTime.now - 1 },
-    { id: 2, name: 'Screwdriver', checked_out_date: DateTime.now - 2 },
-    { id: 3, name: 'Wrench', checked_out_date: DateTime.now - 3 },
-    { id: 4, name: 'Pliers', checked_out_date: DateTime.now - 4 },
-    { id: 5, name: 'Saw', checked_out_date: DateTime.now - 5 }
+    { id: 1, name: "Hammer", checked_out_date: DateTime.now - 1 },
+    { id: 2, name: "Screwdriver", checked_out_date: DateTime.now - 2 },
+    { id: 3, name: "Wrench", checked_out_date: DateTime.now - 3 },
+    { id: 4, name: "Pliers", checked_out_date: DateTime.now - 4 },
+    { id: 5, name: "Saw", checked_out_date: DateTime.now - 5 },
   ]
 
-	erb :'engineering_garage/checkout', :layout => :fixed, :locals => {
-    :projects => user_projects,
-    :checked_out => user_checked_out
-  }
+  erb :'engineering_garage/checkout', :layout => :fixed, :locals => {
+                                    :projects => user_projects,
+                                    :checked_out => user_checked_out,
+                                  }
 end
 
-get '/checkout/new_project/user?' do
-	@breadcrumbs << {:text => 'New_Project'}
-	require_login
+get "/checkout/new_project/user?" do
+  @breadcrumbs << { :text => "New_Project" }
+  require_login
 
-	erb :'engineering_garage/new_project_user_confirmation', :layout => :fixed, :locals => {
-  }
+  erb :'engineering_garage/new_project_user_confirmation', :layout => :fixed, :locals => {}
 end
 
-post '/checkout/new_project/user?' do
-	@breadcrumbs << {:text => 'New_Project'}
-	require_login
-  redirect '/checkout/new_project/create?'
+post "/checkout/new_project/user?" do
+  @breadcrumbs << { :text => "New_Project" }
+  require_login
+  redirect "/checkout/new_project/create?"
 end
 
-get '/checkout/new_project/create?' do
-	@breadcrumbs << {:text => 'New_Project'}
-	require_login
+get "/checkout/new_project/create?" do
+  @breadcrumbs << { :text => "New_Project" }
+  require_login
 
-	erb :'engineering_garage/new_project', :layout => :fixed, :locals => {
-  }
+  erb :'engineering_garage/new_project', :layout => :fixed, :locals => {}
 end
 
-post '/checkout/new_project/create?' do
-	@breadcrumbs << {:text => 'New_Project'}
-	require_login
-  redirect '/checkout/?'
+post "/checkout/new_project/create?" do
+  @breadcrumbs << { :text => "New_Project" }
+  require_login
+  redirect "/checkout/?"
 end
-
