2cae3f8b58afd630186dd57451ac6aa2462d7a8f
Ethan <ethaneckhardt58@gmail.com>
2024-11-21 15:18:57 -0600

    fix db script syntax

--- data/db_test.sql
+++ data/db_test.sql
@@ -3288,7 +3288,7 @@ CREATE TABLE `vehicles` (
 --
 
 CREATE TABLE `projects` (
-  `id` int(11),
+  `id` int(11) NOT NULL,
   `owner_user_id` int(11) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   `description` text DEFAULT NULL,
@@ -3296,7 +3296,7 @@ CREATE TABLE `projects` (
   `last_checked_in` datetime DEFAULT NULL,
   `last_checked_out` datetime DEFAULT NULL,
   `created_on` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_on` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_on` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
