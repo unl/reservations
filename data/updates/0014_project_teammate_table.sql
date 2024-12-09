e599203fbcfdbed93d99810797f19dc8427e09bd
erohrs2 <erohrs2@huskers.unl.edu>
2024-12-09 16:50:40 -0600

    Project Teammates updated to cascade

--- data/updates/0014_project_teammate_table.sql
+++ data/updates/0014_project_teammate_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE `project_teammates` (
   `project_id` int(11) DEFAULT NULL,
   `created_by` int(11) DEFAULT NULL,
   `created_on` datetime DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (teammate_id) REFERENCES users(id),
-  FOREIGN KEY (project_id) REFERENCES projects(id),
-  FOREIGN KEY (created_by) REFERENCES users(id)
+  FOREIGN KEY (teammate_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
+  FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
