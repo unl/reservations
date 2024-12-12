c1b650b60495ac0eeb6818863c73446ae1d06eee
Ethan <ethaneckhardt58@gmail.com>
2024-12-11 18:45:34 -0600

    updated added tables and matching ruby models

--- data/updates/0016_tool_log.sql
+++ data/updates/0016_tool_log.sql
@@ -5,4 +5,4 @@ CREATE TABLE `tool_logs` (
     `tool_name` varchar(255),
     `checked_date` datetime DEFAULT CURRENT_TIMESTAMP,
     `is_checking_in` boolean
-);
+)ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
