093d791ae4f4602fe391a756208f0863c7e6ac30
Thomas Neumann <thomasneumann21800@gmail.com>
2025-01-21 16:55:52 -0600

    Added inputs for the additional trainers

---
+++ data/updates/0018_addtional_trainers.sql
@@ -0,0 +1,12 @@
+ALTER TABLE `events` ADD COLUMN `trainer_2_id` int(11) DEFAULT NULL AFTER `trainer_id`;
+ALTER TABLE `events` ADD COLUMN `trainer_3_id` int(11) DEFAULT NULL AFTER `trainer_2_id`;
+
+ALTER TABLE `events`
+    ADD KEY `FK_trainer_2_id` (`trainer_2_id`);
+ALTER TABLE `events`
+    ADD KEY `FK_trainer_3_id` (`trainer_3_id`);
+
+ALTER TABLE `events`
+    ADD CONSTRAINT `FK_trainer_2_id` FOREIGN KEY (`trainer_2_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `events`
+    ADD CONSTRAINT `FK_trainer_3_id` FOREIGN KEY (`trainer_3_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
