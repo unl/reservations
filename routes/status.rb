dc85c05da8a9a98205b60ade433d40ce0050c462
jburgher2 <jburgher2@DESKTOP-ROKKR3G>
2025-03-24 16:48:16 -0500

    Added the # of down machines stat.

--- routes/status.rb
+++ routes/status.rb
@@ -5,14 +5,18 @@ require 'models/event_type'
 require 'models/event_signup'
 require 'models/space_hour'
 require 'erb'
+require 'models/lockout'
 
 get '/status_page/?' do
 	@breadcrumbs << {:text => 'Status Page'}
+
+    lockout_count = Lockout.select(:resource_id).distinct.count
 
     #Will change this later
     inop_tools = Tool.where(:INOP => 1).all.to_a
 	
 	erb :'/engineering_garage/status_page', :layout => :fixed, :locals => {
-        :inop_tools => inop_tools
+        :inop_tools => inop_tools,
+        :lockout_count => lockout_count
     }
 end
