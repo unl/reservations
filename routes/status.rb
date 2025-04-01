99ffc217e52b299e31b2aaa0b9375d91e42ee092
btran27 <btran19@unl.edu>
2025-04-01 15:28:12 -0500

    Completed Logic for Getting Orientation Potential Walk-Ins

--- routes/status.rb
+++ routes/status.rb
@@ -12,9 +12,20 @@ get '/status_page/?' do
 
     lockout_count = Lockout.where('released_on IS NOT NULL AND released_on < ?', Time.now).select(:resource_id).distinct.count
     lockouts = Lockout.where('released_on IS NOT NULL AND released_on < ?', Time.now).includes(:resource).all
+    
+    # Retrieve a list events ID's of events with the Garage Orientation Event Type
+    orientation_ids = Event.where(event_type_id: 12).pluck(:id)
+    # Retrieve a list of event signups based on the orientation ID's (ActiveRecord handles multiple inputs for SQL search)
+    orientation_signups = EventSignup.where(event_id: orientation_ids)
+    # Get the number of people that haven't attended Orientation from the previous query
+    orientation_potentials = orientation_signups.where(attended: false).count
+
 
 	erb :'/engineering_garage/status_page', :layout => :fixed, :locals => {
         :lockout_count => lockout_count,
-        :lockouts => lockouts
+        :lockouts => lockouts,
+        :orientation_potentials => orientation_potentials
     }
 end
+# events > event_type = 12
+# Filter event signups for all event ids for 
