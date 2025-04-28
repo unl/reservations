4aed4afe145a61899e3ded52565c153ca07fd8bd
Ethan <ethaneckhardt58@gmail.com>
2025-04-28 07:46:33 -0500

    fixed test .sql file

--- data/db_test.sql
+++ data/db_test.sql
@@ -370,7 +370,8 @@ INSERT INTO `permissions` (`id`, `name`) VALUES
 (8, 'User Access'),
 (9, 'Events Admin Read-only'),
 (10, 'Sub-Super User'),
-(11, 'Manage Checkout');
+(11, 'Manage Checkout'),
+(12, 'Manage Lockout');
 
 -- --------------------------------------------------------
 
@@ -3844,6 +3845,21 @@ CREATE TABLE `tools` (
 	`last_checked_out` datetime DEFAULT NULL,
 	`created_on` datetime DEFAULT CURRENT_TIMESTAMP,
 	`updated_on` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+CREATE TABLE `lockouts` (
+	`id` int(11) PRIMARY KEY AUTO_INCREMENT,
+	`resource_id` int(11) NOT NULL,
+	`initiated_by_user_id` int(11) DEFAULT NULL,
+	`description` VARCHAR(255) DEFAULT NULL,
+	`started_on` datetime DEFAULT CURRENT_TIMESTAMP,
+	`released_on` datetime DEFAULT NULL,
+	`released_by_user_id` int(11) DEFAULT NULL,
+	`created_on` datetime DEFAULT CURRENT_TIMESTAMP,
+	`updated_on` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	FOREIGN KEY (resource_id) REFERENCES resources(id) ON DELETE CASCADE,
+	FOREIGN KEY (initiated_by_user_id) REFERENCES users(id) ON DELETE CASCADE,
+	FOREIGN KEY (released_by_user_id) REFERENCES users(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
